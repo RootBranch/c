5926ab295eb18807f361e4d1cbfed62ce8eb598b
RootBranch <113826236+RootBranch@users.noreply.github.com>
2022-10-21 15:27:38 +0800

    P3741 honoka的键盘

--- P3741 honoka的键盘.cpp
+++ P3741 honoka的键盘.cpp
@@ -1,35 +1,55 @@
+//#include <iostream>
+//#include <string.h>
+//using namespace std;
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	string s;
+//	cin >>s;
+//	int ans = 0;
+//	for (int i = 0; i < n - 1; i++)
+//	{
+//		if (s[i] == 'V' && s[i + 1] == 'K')
+//		{
+//			ans++;
+//			s[i] = 'M';
+//			s[i + 1] = 'M';
+//		}
+//	}
+//	for (int i = 0; i < n - 1; i++)
+//	{
+//		if (s[i] == 'V' && s[i + 1] == 'V')
+//		{
+//			ans++;
+//			break;
+//		}
+//		if (s[i] == 'K' && s[i + 1] == 'K')
+//		{
+//			ans++;
+//			break;
+//		}
+//	}
+//	cout << ans;
+//	return 0;
+//}
 #include <iostream>
-#include <string.h>
+#include <string>
 using namespace std;
 int main()
 {
-	int n;
-	cin >> n;
 	string s;
-	cin >>s;
-	int ans = 0;
-	for (int i = 0; i < n - 1; i++)
+	cin >> s;
+	int boy=0;
+	int girl=0;
+	for (int i = 0; i < s.length() - 2; i++)
 	{
-		if (s[i] == 'V' && s[i + 1] == 'K')
-		{
-			ans++;
-			s[i] = 'M';
-			s[i + 1] = 'M';
-		}
+		if (s[i]=='b'||s[i + 1] == 'o' || s[i + 2] == 'y') boy++;
 	}
-	for (int i = 0; i < n - 1; i++)
+	for (int i = 0; i < s.length() - 3; i++)
 	{
-		if (s[i] == 'V' && s[i + 1] == 'V')
-		{
-			ans++;
-			break;
-		}
-		if (s[i] == 'K' && s[i + 1] == 'K')
-		{
-			ans++;
-			break;
-		}
+		if (s[i] == 'g' || s[i + 1] == 'i' || s[i + 2] == 'r'||s[i+3]=='l') girl++;
 	}
-	cout << ans;
+	cout << boy << endl << girl;
 	return 0;
 }
