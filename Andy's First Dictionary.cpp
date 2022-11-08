ae6c3be0c649c97bd0280ef94011cd7132bdebbb
RootBranch <113826236+RootBranch@users.noreply.github.com>
2022-11-08 16:58:49 +0800

    语法

--- Andy's First Dictionary.cpp
+++ Andy's First Dictionary.cpp
@@ -1,27 +1,29 @@
 //https://vjudge.d0j1a1701.cc/contest/528118#problem/H
 #include <iostream>
-#include <string>
+#include <queue>
 #include <set>
 #include <algorithm>
 #include <sstream>
+#include <string>
 using namespace std;
 int main()
 {
-	set<string>se;
-	string s;
-	while (cin >> s)
+	set<string>s;
+	string fir;
+	while (cin >> fir&&fir!="0")
 	{
-		for (int i = 0; i < s.length(); i++)
+		int len = fir.length();
+		for (int i = 0; i < len; i++)
 		{
-			if (isalpha(s[i]))
-				s[i] = tolower(s[i]);
-			else s[i] = ' ';
+			if (isalpha(fir[i]))fir[i] = tolower(fir[i]);
+			else fir[i] = ' ';
 		}
 		string ss;
-		stringstream sss(s);
-		while (sss >> ss)
-			se.insert(ss);
+		stringstream a(fir);
+		while (a >> ss)
+			s.insert(ss);
 	}
-	for (auto i = se.begin(); i != se.end(); i++)
+	for (auto i = s.begin(); i != s.end(); i++)
 		cout << *i << endl;
+	return 0;
 }
