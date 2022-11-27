f4527183a0eb64fb1e72c42de5a13191b11417a8
RootBranch <113826236+RootBranch@users.noreply.github.com>
2022-11-27 20:51:32 +0800

    Add files via upload

---
+++ yufa/博弈论.cpp
@@ -0,0 +1,13 @@
+//https://ac.nowcoder.com/acm/contest/48404/C
+#include <iostream>
+using namespace std;
+int main()
+{
+	long long int n;
+	cin >> n;
+	if (n % 4 == 0)
+		cout << "bob";
+	else
+		cout << "kiki";
+	return 0;
+}
